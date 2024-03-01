b5334ec5ae26197347bd69f097bf489bbbc07f0a
vzanimonets <vzanimonets@gmail.com>
2024-03-01 13:10:19 +0300

    fix: Add env to cookie token

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -12,7 +12,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		domain: 'localhost',
+		domain: process.env.CLIENT_DOMAIN,
 		sameSite: 'strict',
 		expires: 1
 	})
